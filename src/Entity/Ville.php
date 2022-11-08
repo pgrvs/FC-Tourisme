0cac2871d26cd3381807071be874b0b67b2b93b8
Paul Gravinese <gravinese.paul@orange.fr>
2022-11-08 15:49:57 +0100

    creation de bdd

---
+++ src/Entity/Ville.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VilleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VilleRepository::class)]
+class Ville
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 5)]
+    private ?string $codePostal = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $nomDepartement = null;
+
+    #[ORM\Column(length: 2)]
+    private ?string $numDepartement = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $nomRegion = null;
+
+    #[ORM\OneToMany(mappedBy: 'ville', targetEntity: Etablissement::class)]
+    private Collection $etablissements;
+
+    public function __construct()
+    {
+        $this->etablissements = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getCodePostal(): ?string
+    {
+        return $this->codePostal;
+    }
+
+    public function setCodePostal(string $codePostal): self
+    {
+        $this->codePostal = $codePostal;
+
+        return $this;
+    }
+
+    public function getNomDepartement(): ?string
+    {
+        return $this->nomDepartement;
+    }
+
+    public function setNomDepartement(string $nomDepartement): self
+    {
+        $this->nomDepartement = $nomDepartement;
+
+        return $this;
+    }
+
+    public function getNumDepartement(): ?string
+    {
+        return $this->numDepartement;
+    }
+
+    public function setNumDepartement(string $numDepartement): self
+    {
+        $this->numDepartement = $numDepartement;
+
+        return $this;
+    }
+
+    public function getNomRegion(): ?string
+    {
+        return $this->nomRegion;
+    }
+
+    public function setNomRegion(string $nomRegion): self
+    {
+        $this->nomRegion = $nomRegion;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Etablissement>
+     */
+    public function getEtablissements(): Collection
+    {
+        return $this->etablissements;
+    }
+
+    public function addEtablissement(Etablissement $etablissement): self
+    {
+        if (!$this->etablissements->contains($etablissement)) {
+            $this->etablissements->add($etablissement);
+            $etablissement->setVille($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEtablissement(Etablissement $etablissement): self
+    {
+        if ($this->etablissements->removeElement($etablissement)) {
+            // set the owning side to null (unless already changed)
+            if ($etablissement->getVille() === $this) {
+                $etablissement->setVille(null);
+            }
+        }
+
+        return $this;
+    }
+}
