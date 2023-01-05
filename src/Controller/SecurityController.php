fa08532de717542c599e355d11749f7d5832375e
pgrvs <gravinese.paul@orange.fr>
2023-01-05 21:52:11 +0100

    Déconnexion

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -20,5 +21,11 @@ class SecurityController extends AbstractController
             'last_username' => $lastUsername,
             'error'         => $error,
         ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): Response
+    {
+        return $this->redirectToRoute('app_accueil');
     }
 }
