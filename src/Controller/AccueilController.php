f87afc0c21df06eb8fd15d6c8056085e4a34a531
pgrvs <gravinese.paul@orange.fr>
2023-01-11 18:48:56 +0100

    mise a jour des favoris

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
+use App\Repository\EtablissementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
 
 class AccueilController extends AbstractController
 {
-    #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    private EtablissementRepository $etablissementRepository;
+
+    /**
+     * @param EtablissementRepository $etablissementRepository
+     */
+    public function __construct(EtablissementRepository $etablissementRepository)
     {
+        $this->etablissementRepository = $etablissementRepository;
+    }
+
+    #[Route('/', name: 'app_accueil')]
+    public function getEtablissementsAccueil(PaginatorInterface $paginator, Request $request): Response
+    {
+        $etablissements = $paginator->paginate(
+            $this->etablissementRepository->findBy(["actif" => true, "accueil" => true], ['nom' => 'ASC']),
+            $request->query->getInt('page', 1), /*page number*/
+            12 /*limit per page*/
+        );
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+            'etablissements' => $etablissements,
         ]);
     }
 }
