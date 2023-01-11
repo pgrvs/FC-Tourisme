b0310ffc20c74e6e1be2c6976186ca9f5032cfaa
Paul Gravinese <gravinese.paul@orange.fr>
2023-01-11 09:19:10 +0100

    deleteFavori

--- src/Controller/FavoriController.php
+++ src/Controller/FavoriController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Repository\EtablissementRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,15 +26,20 @@ class FavoriController extends AbstractController
         $this->entityManager = $entityManager;
     }
 
-    #[Route('/favoris', name: 'app_favori')]
-    public function index(): Response
+    #[Route('/favoris', name: 'app_favoris')]
+    public function favoris(PaginatorInterface $paginator, Request $request): Response
     {
+        $etablissements = $paginator->paginate(
+            $this->getUser()->getFavoris(),
+            $request->query->getInt('page', 1), /*page number*/
+            12 /*limit per page*/
+        );
         return $this->render('favori/index.html.twig', [
-            'controller_name' => 'FavoriController',
+            'etablissements' => $etablissements,
         ]);
     }
 
-    #[Route('/favoris/{slug}', name: 'app_add_favori_slug')]
+    #[Route('/favoris/ajout/{slug}', name: 'app_add_favori_slug')]
     public function addFavori($slug): Response
     {
         $user = $this->getUser();
@@ -43,4 +50,14 @@ class FavoriController extends AbstractController
         return $this->redirectToRoute('app_etablissements');
     }
 
+    #[Route('/favoris/supprime/{slug}', name: 'app_delete_favori_slug')]
+    public function deleteFavori($slug): Response
+    {
+        $user = $this->getUser();
+        $etablissemnt = $this->etablissementRepository->findOneBy(['slug' => $slug]);
+        $user->removeFavori($etablissemnt);
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+        return $this->redirectToRoute('app_etablissements');
+    }
 }
